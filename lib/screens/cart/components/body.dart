335b95564a030cc150d023cb1aa78d1c8c8ea0f7
Faradila Wahyu Wijayanti <90230447+Faradilala@users.noreply.github.com>
2022-05-23 10:14:19 +0700

    Membuat halaman riwayat

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:proyek1/models/Cart.dart';
-
-import '../../../size_config.dart';
+import 'package:proyek1/screens/cart/components/check_out_card.dart';
 import 'cart_card.dart';
 
 class Body extends StatefulWidget {
@@ -13,37 +10,46 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: ListView.builder(
-        itemCount: demoCarts.length,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Dismissible(
-            key: Key(demoCarts[index].product.id.toString()),
-            direction: DismissDirection.endToStart,
-            onDismissed: (direction) {
-              setState(() {
-                demoCarts.removeAt(index);
-              });
-            },
-            background: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFE6E6),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                children: [
-                  Spacer(),
-                  SvgPicture.asset("assets/icons/Trash.svg"),
-                ],
-              ),
-            ),
-            child: CartCard(cart: demoCarts[index]),
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          CheckoutCard(),
+          // SizedBox(height: 20),
+          Text("15 MEI 2022"),
+          CartCard(
+            text: "Melakukan pembayaran PBB\nPembayaran berhasil",
+            text1: "18:47",
+            press: () => {},
           ),
-        ),
+          CartCard(
+            text: "Mengajukan surat perubahan KK\nPengajuan berhasil terkirim",
+            text1: "11:04",
+            press: () {},
+          ),
+          CartCard(
+            text: "Mengajukan surat perubahan KK\nPengajuan gagal terkirim",
+            text1: "09:12",
+            press: () {},
+          ),
+          Text("02 MAR 2022"),
+          CartCard(
+            text: "Melakukan pendaftaran vaksinasi\nPendaftaran berhasil",
+            text1: "14:16",
+            press: () {},
+          ),
+          CartCard(
+            text: "Mendownload bukti pengajuan\nFile berhasil diunduh",
+            text1: "08:45",
+            press: () {},
+          ),
+          Text("29 FEB 2022"),
+          CartCard(
+            text: "Mengajukan SKTM\nPengajuan berhasil terkirim",
+            text1: "20:01",
+            press: () {},
+          ),
+        ],
       ),
     );
   }
